f33df57289fbb165c2c8535a0f2f897859b521a8
mariifagnds <fagundesmari.senac@gmail.com>
2026-03-03 14:45:28 -0300

    Add files via upload

---
+++ repetição1.py
@@ -0,0 +1,15 @@
+print("Olá, mundo!")
+print("Olá, mundo!")
+print("Olá, mundo!")
+print("Olá, mundo!")
+
+cont = 1
+while cont <=10:
+    print("Olá, mundo!")
+    cont += 1 # cont = cont + 1
+    pint("FIM")
+
+    print(list(range(10)))
+    print(list(range(1,10,2)))
+
+    for cont in range(10): #para cada termo no intervalo de 1 a 10 imprima "Olá mundo!"
